9e14bd71531985dbfe2a9a79193c3d4452d6134e
TomasPrchal <tomas.prchal@icord.cz>
2016-08-16 10:32:27 +0200

    Add files

---
+++ Web/webpack.config.js
@@ -0,0 +1,16 @@
+﻿/// <binding ProjectOpened='Watch - Development' />
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, 'wwwroot/App'),
+  entry: './start.ts',
+  output: {
+    path: path.join(__dirname, 'wwwroot/js'),
+    filename: 'kurentoWrapper.js'
+  },
+  devServer: {
+    contentBase: ".",
+    host: "localhost",
+    port: 9000
+  },
+};
