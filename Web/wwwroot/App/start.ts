0b155519f03e8370ea72f2b678858135dc5cccb0
TomasPrchal <tomas.prchal@icord.cz>
2016-08-17 13:45:19 +0200

    Tweaks

--- Web/wwwroot/App/start.ts
+++ Web/wwwroot/App/start.ts
@@ -6,7 +6,9 @@ function startTheApp() {
   load();
 }
 
+//http://doc-kurento.readthedocs.io/en/stable/_static/langdoc/jsdoc/kurento-client-js/index.html
 let kurentoClient: any = window["kurentoClient"];
+//http://doc-kurento.readthedocs.io/en/stable/_static/langdoc/jsdoc/kurento-utils-js/index.html
 let kurentoUtils: any = window["kurentoUtils"];
 
 let _id = {
@@ -16,7 +18,9 @@ let _id = {
 }
 
 var args = {
-  ws_uri: 'wss://kurento.applicloud.com/kurento',
+  ws_uri: 'wss://kurento.applicloud.com:8433/kurento',
+  //ws_uri: 'wss://nope.com/kurento',
+  file_uri: 'file:///tmp/tomasRecord.webm',
   ice_servers: null
 };
 
@@ -24,7 +28,7 @@ function setIceCandidateCallbacks(webRtcPeer, webRtcEp, onerror) {
   webRtcPeer.on('icecandidate', function (candidate) {
     console.log("Local candidate:", candidate);
 
-    candidate = kurentoClient.getComplexType('IceCandidate')(candidate);
+    candidate = kurentoClient.register.complexTypes.IceCandidate(candidate);
 
     webRtcEp.addIceCandidate(candidate, onerror)
   });
@@ -64,25 +68,44 @@ function load() {
     console.log("Use freeice")
   }
 
+  // http://doc-kurento.readthedocs.io/en/stable/mastering/kurento_utils_js.html
+  // Creates RTCPeerConnection, invokes getUserMedia, but NO connection YET  
   webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(options, function (error) {
     if (error) return onError(error)
 
+    // SDP generated, handled in callback
     this.generateOffer(onOffer)
   });
 
+  // Handles connection and negotiation
   function onOffer(error, sdpOffer) {
     if (error) return onError(error)
 
+    //http://doc-kurento.readthedocs.io/en/stable/_static/langdoc/jsdoc/kurento-client-js/module-kurentoClient.KurentoClient.html
+    // Creates connection to Kurento Media Server
     kurentoClient(args.ws_uri, function (error, client) {
       if (error) return onError(error);
 
+      // http://doc-kurento.readthedocs.io/en/stable/_static/langdoc/jsdoc/kurento-client-js/module-core.MediaPipeline.html
+      // The AV manager object, controlling what happens on Server
       client.create("MediaPipeline", function (error, _pipeline) {
         if (error) return onError(error);
 
         pipeline = _pipeline;
 
-        pipeline.create("WebRtcEndpoint", function (error, webRtc) {
+        var elements =
+          [
+            { type: 'RecorderEndpoint', params: { uri: args.file_uri } },
+            { type: 'WebRtcEndpoint', params: {} }
+          ]
+
+        // http://doc-kurento.readthedocs.io/en/stable/_static/langdoc/jsdoc/kurento-client-js/module-elements.WebRtcEndpoint.html
+        // Add enpoint for the user
+        pipeline.create(elements, function (error, elements) {
           if (error) return onError(error);
+
+          let recorder = elements[0];
+          let webRtc = elements[1];
 
           setIceCandidateCallbacks(webRtcPeer, webRtc, onError)
 
@@ -99,6 +122,8 @@ function load() {
             console.log("Loopback established");
           });
         });
+
+
       });
     });
   }
